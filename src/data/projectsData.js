ce776d5b2daa348b4831c7a99ec56a8e0e163ca0
Osama <osama.nhf@gmail.com>
2026-05-05 23:50:54 +0300

    added projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,26 +1,82 @@
 export const projects = [
     {
+        id: 3,
+        title: "بوت السيرة الذاتية",
+        description: "بوت يراجع سيرتي الذاتية ويجاوب بناء عليها",
+        tags: ["React", "Nginx", "n8n"],
+        link: "chat.osama-alfahad.com",
+        image: "/projects/chat.png"
+    },
+    {
         id: 1,
-        title: "كويزات الجامعة",
+        title: "كويزات تدريبية",
         description: "كويزات تدريبية للاختبارات في الجامعة",
-        tags: ["React", "Tailwind CSS", "ExpressJS", 'SQLite3', "Nginx"],
+        tags: ["React", "ExpressJS", 'SQLite3', "Full Stack"],
         link: "uni-quiz.osama-alfahad.com",
         image: "/projects/uni-quiz.png"
+    },
+    {
+        id: 5,
+        title: "Jira Auto-Monitor",
+        description: "نظام مراقبة آلي ألغى الحاجة للمراجعة اليدوية المتكررة , مما وفر تغطية للموقع بنسبة 100% دون تدخل بشري.",
+        tags: ["Automation", "n8n","Teams Workflows"],
+        image: "/projects/jira-auto.png"
+    },
+    {
+        id: 6,
+        title: "Internal CMS & Doc Sync",
+        description: "منصة معلومات مركزية بديلة لتناقل ملفات Word/PDF، تتيح التحديث الفوري للبيانات وظهورها لجميع الموظفين في نفس اللحظة.",
+        tags: ["Web App", "PHP", "MySQL", "JavaScript"],
+        image: "/projects/cms.png"
+    },
+    {
+        id: 13,
+        title: "Invoice to Saber",
+        description: "اداة لتحويل الفواتير بصيغة PDF إلى ملفات Excel مرتبة وجاهزة للاستخدام في نظام سابر, وهذا يوفر تقريبا 30د لكل فاتورة",
+        tags: ["Automation", "Python"],
+        image: "/projects/saber.png"
+    },
+    {
+        id: 7,
+        title: "Queue Messages Deep Search",
+        description: "أداة بحث وفرت 120 ساعة عمل شهريا تقريبا",
+        tags: ["Automation", "File Systems"],
+        image: "/projects/queue.png"
+    },
+    {
+        id: 8,
+        title: "Integration Visualizer",
+        description: "نظام تتبع للرسائل وملفات الربط، حول عملية البحث المعقدة بين الأقسام إلى استعلام فوري, موفرا 30-60 دقيقة لكل استعلام",
+        tags: ["Data Visualization", "Frontend", "Oracle", "PHP", "JavaScript"],
+        image: "/projects/integration-visual.png"
+    },
+    {
+        id: 10,
+        title: "Smart Task Delegation",
+        description: "نظام إدارة مهام ذكي يقوم بتحويل المهام تلقائياً للموظف البديل أثناء الإجازات لضمان استمرارية العمل.",
+        tags: ["Workflow Logic", "HR Tech", "React", "Automation", "n8n"],
+        image: "/projects/task-delegator.png"
+    },
+    {
+        id: 11,
+        title: "VPS File Search",
+        description: "أداة للبحث السريع داخل السيرفرات، وفرت ما يقارب ساعة عمل يومياً.",
+        tags: ["Linux", "Bash", "Automation"],
+        image: "/projects/vps-search.png"
+    },
+    {
+        id: 12,
+        title: "Auto Ticket Resolution",
+        description: "نظام المعالجة الفورية لتذاكر العملاء، قلل وقت العمل على التذكرة من متوسط 18 ساعة إلى ثواني",
+        tags: ["Automation", "Customer Support", "n8n"],
+        image: "/projects/ticket-auto.png"
     },
     {
         id: 2,
         title: "اختبار شخصية DISC",
         description: "مجموعة من الاسئلة توضح الشخصية الغالبة فيك من تصنيف DISC مع النسبة لكل شخصية",
-        tags: ["React", "Tailwind CSS"],
+        tags: ["React", "Front End"],
         link: "disc-test.osama-alfahad.com",
         image: "/projects/disc.png"
-    },
-    {
-        id: 3,
-        title: "بوت السيرة الذاتية",
-        description: "بوت يراجع سيرتك الذاتية ويجاوب بناء عليها",
-        tags: ["React", "Tailwind CSS", "Nginx", "n8n"],
-        link: "chat.osama-alfahad.com",
-        image: "/projects/chat.png"
     },
 ];
